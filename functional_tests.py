1ecec29075479ae52b095b990ca8c91107154963
Timofeev Konstantin <timofeev.konstantin@clover.global>
2019-12-24 10:09:14 +0300

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,40 +1,54 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-	'''тест нового посетителя'''
 
-	def setUp(self): 
-		'''установка'''
-
+	def setUp(self):
+		"""установка"""
 		self.browser = webdriver.Firefox()
 
-	def tearDown(self): 
-		'''демонтаж'''
+	def tearDown(self):
+		"""демонтаж"""
 
 		self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self): 
-		'''тест: можно начать список и получить его позже'''
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		"""тест: можно начать список и получить его позже"""
 
-		# Эдит слышала про крутое новое онлайн-приложение со
-		# списком неотложных дел. Она решает оценить его
-		# домашнюю страницу
+		# Эдит слышала про крутое новое онлайн-приложение со списком
+		# неотложных дел. Она решает оценить его домашнюю страницу
 		self.browser.get('http://localhost:8000')
-		# Она видит, что заголовок и шапка страницы говорят о
-		# списках неотложных дел
-		self.assertIn('To-Do', self.browser.title) 
-		self.fail('Закончить тест!') 
+		# Она видит, что заголовок и шапка страницы говорят о списках
+		# неотложных дел
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 		# Ей сразу же предлагается ввести элемент списка
-		# Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
-		# вязание рыболовных мушек)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		#Она набирает в текстовом поле "Купить павлиньи перья"
+		# (ее хобби – вязание рыболовных мушек)
+		inputbox.send_keys('Купить павлиньи перья')
 		# Когда она нажимает enter, страница обновляется, и теперь страница
 		# содержит "1: Купить павлиньи перья" в качестве элемента списка
-		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Купить павлиньи перья' for row in rows)
+		)
+		# Текствое поле по-прежнему приглашает ее добавить еще один элемент.
 		# Она вводит "Сделать мушку из павлиньих перьев"
 		# (Эдит очень методична)
-		# Страница снова обновляется, и теперь показывает оба элемента ее списка
+		self.fail('Закончить тест!')
+		# Страница снова обновляется и теперь показывает оба элемента ее списка
 		# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 		# сайт сгенерировал для нее уникальный URL-адрес – об этом
 		# выводится небольшой текст с объяснениями.
@@ -44,4 +58,3 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
-
