a8660fa93199ba1210f998f75230b53801eb2c3a
Timofeev Konstantin <timofeev.konstantin@clover.global>
2019-12-30 11:09:56 +0300

    Рефакторизовано представления домашней страницы для использования шаблона

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
 
 
 # Создайте ваши представления здесь.
-def home_page(request: HttpRequest):
-    """домашняя страниц"""
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+def home_page(request):
+    """домашняя страница"""
+    return render(request, 'home.html')
